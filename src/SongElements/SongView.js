6f9295ff6174818b9939ca8ed7e0997f4cff95f6
Kevin Arthur <kevin.arthur.roy@gmail.com>
2018-05-20 22:41:10 -0700

    [refactor] move autoplay state from App to SongView

--- src/SongElements/SongView.js
+++ src/SongElements/SongView.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import SongCard from './SongCard';
+import ToggleSwitch from '../ToggleSwitch';
 
-const SongView = ({ song, autoplayEnabled }) => (
-  <div>
-    <SongCard song={song} key={song.id} autoplayEnabled={autoplayEnabled} />
-  </div>
-);
+class SongView extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { autoplay: false };
+  }
+
+  toggleAutoplay = () => {
+    this.setState(
+      {
+        autoplay: !this.state.autoplay,
+      },
+      () => console.log(this.state),
+    );
+  };
+
+  render() {
+    const { song, currentProblem } = this.props;
+    const { autoplay } = this.state;
+    if (Object.keys(song).length === 0) {
+      return <div />;
+    } else {
+      return (
+        <div>
+          <ToggleSwitch
+            toggleAutoplay={this.toggleAutoplay}
+            autoplayEnabled={autoplay}
+          />
+          <p>{currentProblem}</p>
+          <SongCard song={song} key={song.id} autoplayEnabled={autoplay} />
+        </div>
+      );
+    }
+  }
+}
 
 export default SongView;
