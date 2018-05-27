4b9cc3c58e2d6498f6bec1f99f8b4263013d8d6d
Kevin Amirdjanian <kevin.amirdjanian@gmail.com>
2018-05-27 13:50:02 -0700

    [visual] update landing view

--- src/SongElements/SongCardDefaultView.js
+++ src/SongElements/SongCardDefaultView.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import './SongCardDefaultView.css';
 
 const SongCardDefaultView = () => (
-  <div className="flex-container-column">
-    <p>
+  <div className="flex-container-column-left-align song-card-default-view">
+    <p className="intro-text">
       Enter a first world problem, listen to a sad song! Let's wallow in our
-      sadness a bit.
+      sadness for a bit.
     </p>
-    <p>
+    <p className="intro-text">
       Too lazy to come up with a problem? Just hit random to submit a random
       problem!
+    </p>
+    <p className="intro-text">
+      Autoplay is disabled by default so as to not accidentally disturb your
+      co-workers. For best effect however, you ought to enable it.
     </p>
   </div>
 );
