5341ecbdd65cee4f83166b5c779b36d98aa45358
Kevin Arthur <kevin.arthur.roy@gmail.com>
2018-05-27 14:20:02 -0700

    [visual] add song info under album art

--- src/SongElements/SongCard.js
+++ src/SongElements/SongCard.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import SongImage from './SongImage';
+import SongData from './SongData';
 import SongControls from './SongControls';
 import SongLink from './SongLink';
 
 const SongCard = ({ song, autoplayEnabled }) => (
   <div className="flex-container-column">
     <SongImage songImage={song.album.images[0].url} />
+    <SongData album={song.album} artists={song.artists} name={song.name} />
     <SongControls
       songUrl={song.preview_url}
       autoplayEnabled={autoplayEnabled}
--- src/SongElements/SongData.js
+++ src/SongElements/SongData.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import './SongData.css';
 
 const SongData = ({ album, artists, name }) => {
-  return <div />;
+  const artistInfo = artists.map(artist => artist.name).join(', ');
+  return (
+    <div className="flex-container-column-left-align song-data">
+      <span className="song-info">Title: {name}</span>
+      <span className="song-info">Artist: {artistInfo}</span>
+      <span className="song-info">Album: {album.name}</span>
+    </div>
+  );
 };
 
 export default SongData;
