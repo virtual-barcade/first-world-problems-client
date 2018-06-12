b6c1402d1daf5b9176721b876ad6b26861fc8819
Kevin Arthur <kevin.arthur.roy@gmail.com>
2018-06-12 16:14:50 -0700

    [test] add jest + enzyme to project

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '../src/App';
+import { shallow } from 'enzyme';
+import App from '../App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders without crashing', () => {
+  shallow(<App />);
 });
